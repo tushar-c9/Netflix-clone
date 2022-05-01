fcca23e2ac03a9f5313782ac8714570924a87ced
Tushar Choudhary <56794051+tusharC6@users.noreply.github.com>
2022-05-01 20:56:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+let list = document.getElementsByClassName("faq_b1");
+
+for(let i=0; i<list.length; i++) {
+    list[i].addEventListener("click",  function(){
+        if (this.childNodes[1].classList.contains("fa-plus")) {
+            this.childNodes[1].classList.remove("fa-plus");
+            this.childNodes[1].classList.add("fa-times");    
+        }
+        else {
+            this.childNodes[1].classList.remove("fa-times");
+            this.childNodes[1].classList.add("fa-plus"); 
+        }
+
+        let content = this.nextElementSibling;
+        if(content.style.maxHeight) {
+            content.style.maxHeight = null; 
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    });
+}
